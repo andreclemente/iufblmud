6429e7ee384993efc741f2339c601a9f35b8b7bd
bleak <wilson.oliveira@gmail.com>
2012-09-19 21:57:54 +0100

    Added Moment.js. Removed the facebook login dialog.
    Switched the created_time parser to the Moment.js library.
    Changed the facebook dialog to redirect.

--- js/iufblmud.js
+++ js/iufblmud.js
@@ -72,62 +72,16 @@ window.fbAsyncInit = function () {
             feed.accessToken = response.authResponse.accessToken;
             feed.refresh();
         } else {
-            FB.login(function (response) {
-                if (response.authResponse) {
-                    feed.accessToken = response.authResponse.accessToken;
-                    feed.refresh();
-                }
-            }, { scope: 'read_stream' });
+            // FB.login(function (response) {
+            //     if (response.authResponse) {
+            //         feed.accessToken = response.authResponse.accessToken;
+            //         feed.refresh();
+            //     }
+            // }, { scope: 'read_stream' });
+            window.location = "https://www.facebook.com/dialog/oauth/?client_id=145530318799386&redirect_uri=http://woli.github.com/iufblmud/index.html&scope=read_stream";
         }
     });
 };
-
-Date.fromISO= (function(){
-    var testIso = '2011-11-24T09:00:27+0200';
-    // Chrome
-    var diso= Date.parse(testIso);
-    if(diso===1322118027000) return function(s){
-        return new Date(Date.parse(s));
-    }
-    // JS 1.8 gecko
-    var noOffset = function(s) {
-      var day= s.slice(0,-5).split(/\D/).map(function(itm){
-        return parseInt(itm, 10) || 0;
-      });
-      day[1]-= 1;
-      day= new Date(Date.UTC.apply(Date, day));  
-      var offsetString = s.slice(-5)
-      var offset = parseInt(offsetString,10)/100; 
-      if (offsetString.slice(0,1)=="+") offset*=-1;
-      day.setHours(day.getHours()+offset);
-      return day.getTime();
-    }
-    if (noOffset(testIso)===1322118027000) {
-       return noOffset;
-    }  
-    return function(s){
-        var day, tz, 
-        rx = /^(\d{4}\-\d\d\-\d\d([tT][\d:\.]*)?)([zZ]|([+\-])(\d\d):?(\d\d))?$/,
-            
-        p= rx.exec(s) || [];
-        if(p[1]){
-            day= p[1].split(/\D/).map(function(itm){
-                return parseInt(itm, 10) || 0;
-            });
-            day[1]-= 1;
-            day= new Date(Date.UTC.apply(Date, day));
-            if(!day.getDate()) return NaN;
-            if(p[5]){
-                tz= parseInt(p[5], 10)/100*60;
-                if(p[6]) tz += parseInt(p[6], 10);
-                if(p[4]== "+") tz*= -1;
-                if(tz) day.setUTCMinutes(day.getUTCMinutes()+ tz);
-            }
-            return day;
-        }
-        return NaN;
-    }
-})();
 
 function Playlist() {
     this.auto_advance = false;
@@ -213,9 +167,9 @@ Feed.prototype.refresh = function () {
 Feed.prototype.sort = function () {
     this.posts.sort(function (x, y) {
         if (feed.sort_dir == 'desc') {
-            return Date.parse(y.created_time) - Date.parse(x.created_time);
+            return y.created_time - x.created_time;
         } else {
-            return Date.parse(x.created_time) - Date.parse(y.created_time);
+            return x.created_time - y.created_time;
         }
     });
 };
@@ -292,7 +246,7 @@ Post.prototype.init = function (data) {
     this.id = data.id != undefined ? data.id : null;
     this.message = data.message != undefined ? data.message : null;
     this.name = data.name != undefined ? data.name : null;
-    this.created_time = data.created_time != undefined ? new Date(Date.fromISO(data.created_time)) : null;
+    this.created_time = data.created_time != undefined ? moment(data.created_time) : null;
     this.likes = (data.likes != undefined && data.likes.count != undefined) ? data.likes.count : 0;
     this.comments = (data.comments != undefined && data.comments.count != undefined) ? data.comments.count : 0;
 
@@ -324,7 +278,7 @@ Post.prototype.toHtml = function () {
                 '<img class="post-img img-rounded" src="https://graph.facebook.com/' + this.from.id + '/picture">' +
             '</a>' +
             '<div class="post-details">' +
-                '<small class="post-time">' + this.created_time.toLocaleTimeString() + '</small>' +
+                '<small class="post-time">' + this.created_time.format("HH:mm:ss") + '</small>' +
                 '<p class="post-from">' +
                     '<a href="http://www.facebook.com/profile.php?id=' + this.from.id + '">' + this.from.name + '</a>' +
                 '</p>' +
